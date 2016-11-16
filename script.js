b3ede35014d44042aaae49772e1ca75367c2ae47
Björn Husberg <bjorn@husberg.se>
2016-11-16 03:02:22 +0100

    Logo margins

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 
 $(document).ready(function() {
   $( ".zite-logo-letter" ).animate({
-      "margin-right": 4,
-      "margin-left": 0,
+      "margin-right": 2,
+      "margin-left": 2,
       zoom: 1
     }, { queue: false, duration: 1000});
   $( ".zite-logo" )
